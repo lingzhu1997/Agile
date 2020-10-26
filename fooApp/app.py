f385b94b7b0c60eff9adddb680f0256d2defb40a
lingzhu1997 <lingzhu111@hotmail.com>
2020-10-26 09:14:07 +0100

    update app.py

--- fooApp/app.py
+++ fooApp/app.py
@@ -141,38 +141,3 @@ def logout():
   logout_user()
   return redirect(url_for('products_list'))
 
-@app.route('/string/')
-def return_string():
-  dump = dump_request_detail(request)
-  return 'Hello, world!'
-
-@app.route('/object/')
-def return_object():
-  dump = dump_request_detail(request)
-  headers = {'Content-Type': 'text/plain'}
-  return make_response(Response('Hello, world! \n' + dump, status=200,
-    headers=headers))
-
-@app.route('/tuple/<path:resource>')
-def return_tuple(resource):
-  dump = dump_request_detail(request)
-  return 'Hello, world! \n' + dump, 200, {'Content-Type':
-    'text/plain'}
-
-
-def dump_request_detail(request):
-  request_detail = """
-## Request INFO ##
-request.endpoint: {request.endpoint}
-request.method: {request.method}
-request.view_args: {request.view_args}
-request.args: {request.args}
-request.form: {request.form}
-request.user_agent: {request.user_agent}
-request.files: {request.files}
-request.is_xhr: {request.is_xhr}
-
-## request.headers ##
-{request.headers}
-  """.format(request=request).strip()
-  return request_detail
